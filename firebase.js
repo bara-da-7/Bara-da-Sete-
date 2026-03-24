351138f075631d5f13f5d8303698876652921f16
bara-da-7 <baradasete@gmail.com>
2026-03-24 14:07:45 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,8 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA--Yx-rJnPlRFXzMc0GhDzAqNav5bGOLw",
-  authDomain: "bara-da-sete.firebaseapp.com",
-  projectId: "bara-da-sete",
-  storageBucket: "bara-da-sete.firebasestorage.app",
-  messagingSenderId: "162143376770",
-  appId: "1:162143376770:web:4e62e135636a477057eb9b"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if true;
+    }
+  }
+}
