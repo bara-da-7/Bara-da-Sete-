d161881b3215d2cfaca03aeadc8fcf0987110b9d
bara-da-7 <baradasete@gmail.com>
2026-03-24 15:02:13 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,20 @@
-self.addEventListener("install", e=>{
-  self.skipWaiting();
+self.addEventListener('install', e=>{
+ e.waitUntil(
+  caches.open('v1').then(cache=>{
+   return cache.addAll([
+    '/',
+    '/index.html',
+    '/estilo.css',
+    '/script.js'
+   ]);
+  })
+ );
+});
+
+self.addEventListener('fetch', e=>{
+ e.respondWith(
+  caches.match(e.request).then(res=>{
+   return res || fetch(e.request);
+  })
+ );
 });
