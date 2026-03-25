c5eddd0b3d6e4e303592c4fc96016b93d4f746f6
bara-da-7 <baradasete@gmail.com>
2026-03-25 13:47:01 -0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,18 +1,18 @@
-function render(produtos){
- const div = document.getElementById("produtos")
- div.innerHTML=""
+function render(lista){
+ produtosDiv.innerHTML=""
 
- produtos.forEach(p=>{
-  if(p.ativo !== "sim") return
+ lista.forEach(p=>{
+  if(String(p.ativo).toLowerCase() !== "sim") return
 
-  let preco = p.promocao==="sim" ? p.precoPromo : p.preco
+  let preco = p.promocao==="sim" && p.precoPromo>0 ? p.precoPromo : p.preco
 
-  div.innerHTML += `
+  produtosDiv.innerHTML += `
   <div class="card">
     <img src="${p.imagem}">
     <h3>${p.nome}</h3>
+    <p>${p.descricao || ""}</p>
     <p class="preco">R$ ${preco}</p>
-    <button onclick="add('${p.nome}',${preco})">+</button>
+    <button onclick="add('${p.nome}')">+</button>
   </div>`
  })
 }
