a7a0055293880d1fbdc3dbd1e7712f76d4fe29b9
bara-da-7 <baradasete@gmail.com>
2026-03-25 13:26:54 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,39 +2,35 @@ const CLOUD = "dy61d0gt8"
 const PRESET = "ml_padrao"
 
 function logar(){
-if(document.getElementById("senha").value === "99861309"){
-document.getElementById("login").style.display="none"
-document.getElementById("painel").style.display="block"
-}
+ if(user.value==="adm" && senha.value==="99861309"){
+  login.style.display="none"
+  painel.style.display="block"
+ }
 }
 
 function salvar(){
+ let file = imagem.files[0]
 
-let file = document.getElementById("imagem").files[0]
+ let form = new FormData()
+ form.append("file", file)
+ form.append("upload_preset", PRESET)
 
-let form = new FormData()
-form.append("file",file)
-form.append("upload_preset",PRESET)
-
-fetch(`https://api.cloudinary.com/v1_1/${CLOUD}/image/upload`,{
-method:"POST",
-body:form
-})
-.then(r=>r.json())
-.then(img=>{
-
-fetch("SUA API",{
-method:"POST",
-body:JSON.stringify({
-nome: nome.value,
-preco: preco.value,
-categoria: categoria.value,
-imagem: img.secure_url,
-promocao: promo.checked ? "sim":"não",
-precoPromo: precoPromo.value
-})
-})
-
-alert("Salvo")
-})
+ fetch(`https://api.cloudinary.com/v1_1/${CLOUD}/image/upload`,{
+  method:"POST",
+  body:form
+ })
+ .then(r=>r.json())
+ .then(img=>{
+  fetch(API,{
+   method:"POST",
+   body: JSON.stringify({
+    nome:nome.value,
+    preco:preco.value,
+    categoria:categoria.value,
+    imagem:img.secure_url,
+    ativo:"sim"
+   })
+  })
+  alert("Produto salvo")
+ })
 }
