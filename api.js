aa8370fab38276ccdf73b495e738d748d84cf90b
bara-da-7 <baradasete@gmail.com>
2026-03-25 16:32:02 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,20 +1,19 @@
-const SHEETS_API = "https://script.google.com/macros/s/AKfycbzZ6BsrhgLa1f6nAWTWlWCS5j_cq0xhos5pjHhSgRLUVxJPYIqoeoL-Gy6mCukzCxgz/exec";
+const API = "https://script.google.com/macros/s/AKfycbzZ6BsrhgLa1f6nAWTWlWCS5j_cq0xhos5pjHhSgRLUVxJPYIqoeoL-Gy6mCukzCxgz/exec";
 
 window.api = {
-  async getProdutos() {
-    try {
-      const res = await fetch(SHEETS_API);
-      return await res.json();
-    } catch {
-      return [];
-    }
-  },
-
-  async buscarCep(cep) {
-    cep = cep.replace(/\D/g,'');
-    if(cep.length !== 8) return {};
-
-    const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    return await res.json();
+async produtos(){
+  try{
+    const r = await fetch(API);
+    return await r.json();
+  }catch{
+    return [];
   }
+},
+
+async cep(v){
+  v = v.replace(/\D/g,'');
+  if(v.length!==8) return {};
+  const r = await fetch(`https://viacep.com.br/ws/${v}/json/`);
+  return await r.json();
+}
 };
