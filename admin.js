4132c94731ff39586518f841e9777447f364f8d2
bara-da-7 <baradasete@gmail.com>
2026-03-25 16:26:55 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,33 +1,8 @@
-function logar(){
- if(user.value==="adm" && senha.value==="99861309"){
-  login.style.display="none"
-  painel.style.display="block"
- }
+async function loadAdmin(){
+  const produtos = await api.getProdutos();
+
+  document.getElementById("admin-list").innerHTML =
+    produtos.map(p=>`<div>${p.nome}</div>`).join('');
 }
 
-function salvar(){
- let file = imagem.files[0]
-
- let form = new FormData()
- form.append("file",file)
- form.append("upload_preset","ml_padrao")
-
- fetch("https://api.cloudinary.com/v1_1/dy61d0gt8/image/upload",{
-  method:"POST",
-  body:form
- })
- .then(r=>r.json())
- .then(img=>{
-  fetch(API,{
-   method:"POST",
-   body: JSON.stringify({
-    tipo:"produto",
-    nome:nome.value,
-    preco:preco.value,
-    categoria:categoria.value,
-    imagem:img.secure_url
-   })
-  })
-  alert("Produto salvo!")
- })
-}
+loadAdmin();
