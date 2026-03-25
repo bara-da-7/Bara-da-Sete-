5ea9f771488f11eb41f0d42ad5c4865308b60e87
bara-da-7 <baradasete@gmail.com>
2026-03-25 13:53:02 -0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -5,14 +5,21 @@ function render(lista){
   if(String(p.ativo).toLowerCase() !== "sim") return
 
   let preco = p.promocao==="sim" && p.precoPromo>0 ? p.precoPromo : p.preco
+  let qtd = carrinho[p.nome] || 0
 
   produtosDiv.innerHTML += `
   <div class="card">
     <img src="${p.imagem}">
     <h3>${p.nome}</h3>
-    <p>${p.descricao || ""}</p>
-    <p class="preco">R$ ${preco}</p>
-    <button onclick="add('${p.nome}')">+</button>
+    <p>${p.descricao||""}</p>
+
+    <div class="preco">R$ ${preco}</div>
+
+    <div class="controls">
+      <button onclick="diminuir('${p.nome}')">-</button>
+      <span class="qtd">${qtd}</span>
+      <button onclick="add('${p.nome}')">+</button>
+    </div>
   </div>`
  })
 }
