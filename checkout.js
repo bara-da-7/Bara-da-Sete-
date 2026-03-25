6fadb0969e8fc5d0c7179f027ac089f5f61947bf
bara-da-7 <baradasete@gmail.com>
2026-03-25 11:15:42 -0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,30 +1,12 @@
-let carrinho = JSON.parse(localStorage.getItem("carrinho")) || {};
-
 function finalizar(){
 
-    let nome = document.getElementById("nome").value;
-    let tel = document.getElementById("telefone").value;
+let carrinho = JSON.parse(localStorage.getItem("carrinho")) || {};
 
-    let total = 0;
-    let lista = "";
+let msg = "🛒 Pedido:\n";
 
-    for(let item in carrinho){
-        let qtd = carrinho[item];
-        lista += `• ${item} x${qtd}\n`;
-        total += qtd;
-    }
+for(let item in carrinho){
+msg += `${item} x${carrinho[item]}\n`;
+}
 
-    let msg = `
-🛒 *Novo Pedido - Bará da Se7e*
-
-📦 *Produtos:*
-${lista}
-
-💰 *Total:* ${total}
-
-👤 ${nome}
-📞 ${tel}
-    `;
-
-    window.open(`https://wa.me/5554996169777?text=${encodeURIComponent(msg)}`);
+window.open("https://wa.me/5554996169777?text="+encodeURIComponent(msg));
 }
