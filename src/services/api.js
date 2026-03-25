c4fe250f521760a703c3eeb172df679d43e1f329
bara-da-7 <baradasete@gmail.com>
2026-03-25 20:41:29 -0300

    Delete src directory

--- src/services/api.js
+++
@@ -1,6 +0,0 @@
-const SHEETS_URL = "https://script.google.com/macros/s/AKfycbzZ6BsrhgLa1f6nAWTWlWCS5j_cq0xhos5pjHhSgRLUVxJPYIqoeoL-Gy6mCukzCxgz/exec";
-
-export async function getProdutos() {
-  const res = await fetch(SHEETS_URL);
-  return await res.json();
-}
