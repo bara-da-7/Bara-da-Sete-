0e6057534c932eb2421c3f8a44a95f6f0a426d17
bara-da-7 <baradasete@gmail.com>
2026-03-25 09:49:38 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,9 +1,21 @@
-const CONFIG={
- LOGIN:"adm",
- SENHA:"99861309",
-
- CLOUD_NAME:"dy61d0gt8",
- UPLOAD_PRESET:"ml_padrao",
-
- APPS_SCRIPT:"https://script.google.com/macros/s/AKfycbzJVNHNmRoMNRtBPusHXL04VyphdycYXSGyWMWTgZN0Jv4rf1HLqn7YHtJlPKq8dml8/exec"
+const CLOUD={
+ cloudName:"dy61d0gt8",
+ uploadPreset:"ml_padrao"
 };
+
+const API="https://script.google.com/macros/s/AKfycbzJVNHNmRoMNRtBPusHXL04VyphdycYXSGyWMWTgZN0Jv4rf1HLqn7YHtJlPKq8dml8/exec";
+
+async function upload(file){
+
+ const f=new FormData();
+ f.append("file",file);
+ f.append("upload_preset",CLOUD.uploadPreset);
+
+ const r=await fetch(`https://api.cloudinary.com/v1_1/${CLOUD.cloudName}/image/upload`,{
+  method:"POST",
+  body:f
+ });
+
+ const d=await r.json();
+ return d.secure_url;
+}
