e33f3914f9dde3e2d3cc8b37f822e0d9af0e3f45
bara-da-7 <baradasete@gmail.com>
2026-03-25 16:32:15 -0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,43 +1,32 @@
-window.cart = {
-  items: JSON.parse(localStorage.getItem("cart") || "{}"),
+window.cart={
+items:JSON.parse(localStorage.getItem("cart")||"{}"),
 
-  save() {
-    localStorage.setItem("cart", JSON.stringify(this.items));
-  },
+save(){localStorage.setItem("cart",JSON.stringify(this.items))},
 
-  add(prod) {
-    if(!this.items[prod.nome]) {
-      this.items[prod.nome] = { produto: prod, qtd:0 };
-    }
+add(p){
+if(!this.items[p.nome]) this.items[p.nome]={produto:p,q:0};
+if(this.items[p.nome].q>=p.estoque) return;
+this.items[p.nome].q++;
+this.save();
+},
 
-    if(this.items[prod.nome].qtd >= prod.estoque) return;
+dec(n){
+if(!this.items[n])return;
+this.items[n].q--;
+if(this.items[n].q<=0) delete this.items[n];
+this.save();
+},
 
-    this.items[prod.nome].qtd++;
-    this.save();
-  },
+remove(n){
+delete this.items[n];
+this.save();
+},
 
-  remove(nome) {
-    delete this.items[nome];
-    this.save();
-  },
+total(){
+return Object.values(this.items).reduce((a,i)=>a+i.q,0)
+},
 
-  decrease(nome) {
-    if(!this.items[nome]) return;
-
-    this.items[nome].qtd--;
-
-    if(this.items[nome].qtd <=0) delete this.items[nome];
-
-    this.save();
-  },
-
-  totalQtd() {
-    return Object.values(this.items).reduce((a,i)=>a+i.qtd,0);
-  },
-
-  totalPreco() {
-    return Object.values(this.items).reduce((a,i)=>{
-      return a + parseFloat(i.produto.preco) * i.qtd;
-    },0);
-  }
-};
+price(){
+return Object.values(this.items).reduce((a,i)=>a+i.q*parseFloat(i.produto.preco),0)
+}
+}
