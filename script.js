76211840d3c6a2824e56950f304efe36b23e10ee
bara-da-7 <baradasete@gmail.com>
2026-03-25 13:25:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-const API = "https://script.google.com/macros/s/AKfycbzJVNHNmRoMNRtBPusHXL04VyphdycYXSGyWMWTgZN0Jv4rf1HLqn7YHtJlPKq8dml8/exec"
-  produtos=data
-  render()
-  categorias()
- })
+let produtos = []
+
+async function init(){
+ produtos = await getProdutos()
+ render(produtos)
+ updateCart()
+ gerarCategorias()
 }
 
-function render(lista=produtos){
- produtosDiv.innerHTML=''
- lista.forEach(p=>{
-  if(p.ativo!=='sim')return
-  let preco=p.promocao==='sim'?p.precoPromo:p.preco
-  produtosDiv.innerHTML+=`<div class="card">
-   <img src="${p.imagem}">
-   <h3>${p.nome}</h3>
-   <p class="preco">R$ ${preco}</p>
-   <button onclick="add('${p.nome}',${preco})">+</button>
-  </div>`
- })
-}
+function gerarCategorias(){
+ let cats = [...new Set(produtos.map(p=>p.categoria))]
+ let div = document.getElementById("categorias")
 
-function categorias(){
- let cats=[...new Set(produtos.map(p=>p.categoria))]
- categoriasDiv.innerHTML=''
  cats.forEach(c=>{
-  categoriasDiv.innerHTML+=`<button onclick="filtrar('${c}')">${c}</button>`
+  div.innerHTML += `<button onclick="filtrar('${c}')">${c}</button>`
  })
 }
 
@@ -31,37 +20,28 @@ function filtrar(cat){
  render(produtos.filter(p=>p.categoria===cat))
 }
 
-busca.oninput=(e)=>{
- let t=e.target.value.toLowerCase()
+busca.oninput = e=>{
+ let t = e.target.value.toLowerCase()
  render(produtos.filter(p=>p.nome.toLowerCase().includes(t)))
 }
 
-function add(nome,preco){
- carrinho[nome]=(carrinho[nome]||0)+1
- localStorage.setItem('carrinho',JSON.stringify(carrinho))
+carrinhoBtn.onclick = ()=>{
+ carrinho.classList.toggle("ativo")
+}
+
+async function checkout(){
+ let nome = document.getElementById("nomeCliente").value
+ let endereco = document.getElementById("endereco").value
+
+ let pedido = {nome, endereco, carrinho}
+
+ await salvarPedido(pedido)
+
+ alert("Pedido enviado!")
+
+ carrinho = {}
+ localStorage.clear()
  updateCart()
 }
 
-function updateCart(){
- let total=0,html=''
- for(let i in carrinho){
-  let p=produtos.find(x=>x.nome===i)
-  let preco=p?.preco||0
-  total+=preco*carrinho[i]
-  html+=`<p>${i} x${carrinho[i]}</p>`
- }
- itensCarrinho.innerHTML=html
- totalEl.innerText='Total R$ '+total
- contador.innerText=Object.values(carrinho).reduce((a,b)=>a+b,0)
-}
-
-function finalizar(){
- let msg='Pedido:%0A'
- for(let i in carrinho){
-  msg+=`${i} x${carrinho[i]}%0A`
- }
- window.open(`https://wa.me/5554996169777?text=${msg}`)
-}
-
-carregar()
-updateCart()
+init()
