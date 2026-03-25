b2a7216a4ed0c4ecc9854911e10278a49051f138
bara-da-7 <baradasete@gmail.com>
2026-03-25 07:47:19 -0300

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,9 @@
+const CONFIG={
+ LOGIN:"adm",
+ SENHA:"99861309",
+
+ CLOUD_NAME:"dy61d0gt8",
+ UPLOAD_PRESET:"ml_padrao",
+
+ APPS_SCRIPT:"https://script.google.com/macros/s/AKfycbzJVNHNmRoMNRtBPusHXL04VyphdycYXSGyWMWTgZN0Jv4rf1HLqn7YHtJlPKq8dml8/exec"
+};
