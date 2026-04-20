a39e4784d313ae3973a0799ea332199f44e768c0
bara-da-7 <baradasete@gmail.com>
2026-04-20 09:10:14 -0300

    Update app.js

--- site/app.js
+++ site/app.js
@@ -1,25 +1,32 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxGgjW2-E0wwkcNhRdxk8PtelGe3OXmMRM9USaraHtMGRCyn5niukym3Qr5zbHAFAZ5/exec"
+};
+
 let allProducts = [];
 
 async function loadProducts(){
   const res = await fetch(CONFIG.API_URL + "?action=getProducts");
   allProducts = await res.json();
+
+  document.getElementById("skeleton").style.display = "none";
   renderProducts(allProducts);
 }
 
-function renderProducts(products){
+function renderProducts(list){
   const el = document.getElementById("products");
   el.innerHTML = "";
 
-  products.forEach(p=>{
+  list.forEach(p=>{
     el.innerHTML += `
       <div class="product">
-        <div class="badge">🔥 Promoção</div>
         <img src="${p.img}">
         <h3>${p.name}</h3>
         <p>R$ ${p.price}</p>
 
         <div class="qty">
-          <button onclick="addToCart(${JSON.stringify(p)})">+</button>
+          <button onclick='addToCart(${JSON.stringify(p)})'>+</button>
+          <span>${getQty(p.id)}</span>
+          <button onclick='removeOne(${p.id})'>-</button>
         </div>
       </div>
     `;
@@ -28,8 +35,24 @@ function renderProducts(products){
 
 function filterProducts(){
   const term = document.getElementById("search").value.toLowerCase();
-  const filtered = allProducts.filter(p => p.name.toLowerCase().includes(term));
-  renderProducts(filtered);
+  renderProducts(allProducts.filter(p=>p.name.toLowerCase().includes(term)));
+}
+
+function getQty(id){
+  const item = cart.find(i=>i.id===id);
+  return item ? item.qtd : 0;
+}
+
+let clicks=0;
+function adminClick(){
+  clicks++;
+  if(clicks>=10){
+    const senha=prompt("Senha:");
+    if(senha==="1069"){
+      window.location.href="../admin/admin.html";
+    }
+    clicks=0;
+  }
 }
 
 loadProducts();
